2c58a8d8a5f129913d1ef1becc8003b6de41d913
hangzaizai <249027600@qq.com>
2017-08-09 21:12:50 +0800

    添加CHPlayer类

---
+++ CHPlayer/CHPlayer/Player/CHPlayer.h
@@ -0,0 +1,13 @@
+//
+//  CHPlayer.h
+//  CHPlayer
+//
+//  Created by 陈行 on 17/8/9.
+//  Copyright © 2017年 陈行. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHPlayer : NSObject
+
+@end
---
+++ CHPlayer/CHPlayer/Player/CHPlayer.m
@@ -0,0 +1,13 @@
+//
+//  CHPlayer.m
+//  CHPlayer
+//
+//  Created by 陈行 on 17/8/9.
+//  Copyright © 2017年 陈行. All rights reserved.
+//
+
+#import "CHPlayer.h"
+
+@implementation CHPlayer
+
+@end
