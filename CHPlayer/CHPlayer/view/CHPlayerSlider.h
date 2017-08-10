42b306c5def525c87a9e74d516281cd3a3d7561f
hangzaizai <249027600@qq.com>
2017-08-10 20:24:59 +0800

    引入pureLayout

---
+++ CHPlayer/CHPlayer/view/CHPlayerSlider.h
@@ -0,0 +1,13 @@
+//
+//  CHPlayerSlider.h
+//  CHPlayer
+//
+//  Created by 陈行 on 17/8/10.
+//  Copyright © 2017年 陈行. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHPlayerSlider : UISlider
+
+@end
---
+++ CHPlayer/CHPlayer/view/CHPlayerSlider.m
@@ -0,0 +1,21 @@
+//
+//  CHPlayerSlider.m
+//  CHPlayer
+//
+//  Created by 陈行 on 17/8/10.
+//  Copyright © 2017年 陈行. All rights reserved.
+//
+
+#import "CHPlayerSlider.h"
+
+@implementation CHPlayerSlider
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
